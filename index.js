e63de1345d831eaad863ab0a7bc1bf07ae2dabf0
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-04-26 21:47:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,3 +15,5 @@ const ageIsValid = Number.isInteger(currentAge)
 const randomNumber = Math.floor(Math.random() * 20)
 
 const randomInteger = Math.floor(randomNumber)
+
+const randomUserID = 1000000000 + randomInteger
